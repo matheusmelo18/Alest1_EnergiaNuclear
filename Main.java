ce600632251fb65520829166f7f5d2df0968face
Melo <matheusmelo180203@gmail.com>
2024-09-09 22:08:24 -0300

    primeiro commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        SubEstacao estacao = new SubEstacao();
+
+        try{
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
---
+++ SubEstacao.java
@@ -0,0 +1,7 @@
+public class SubEstacao {
+    private int energia = 0;
+
+
+
+
+}
